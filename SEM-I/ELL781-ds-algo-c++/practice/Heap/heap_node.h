ae6890dcde490912b37cda48a0242c709e7cf2fc
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2024-08-16 06:50:43 +0530

    Add files via upload

---
+++ SEM-I/ELL781-ds-algo-c++/practice/Heap/heap_node.h
@@ -0,0 +1,22 @@
+// header5
+#ifndef HEADER5_H
+#define HEADER5_H
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+// Other declarations and definitions
+
+#endif // HEADER5_H
+#pragma once
+
+// Declarations and definitions
+
+class Heap_Node {
+public:
+    int data;
+    Heap_Node* parent;
+    Heap_Node* leftChild;
+    Heap_Node* rightChild;
+};
